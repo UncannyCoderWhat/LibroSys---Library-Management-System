e4317314d6b043d386bb2ecfd1d702cdc08badff
GM-Isaac <gianmanela05@gmail.com>
2026-05-13 18:58:40 +0800

    Add Updated sidebar and topbar

--- sidebar.php
+++ sidebar.php
@@ -3,22 +3,20 @@
 <div class="sidebar">
 
     <div class="nav-top">
-
-        <h2>LibroSys</h2>
-
         <a href="dashboard.php">Dashboard</a>
         <a href="books.php">Books</a>
         <a href="borrowing.php">Borrowing</a>
         <a href="borrowed.php">Borrowed Books</a>
         <a href="users.php">Users</a>
         <a href="settings.php">Settings</a>
-
     </div>
 
     <div class="nav-bottom">
-
         <a href="logout.php">Logout</a>
-
     </div>
 
+</div>
+
+<div class="topbar">
+    <img src="LibroSys.png" alt="Logo">
 </div>
