8b2ee41fc9285499b2b0f73132e6bd5ed0bd1f5d
GM-Isaac <gianmanela05@gmail.com>
2026-05-16 10:44:13 +0800

    UPDATE: ADD BORROWED BOOKS UI

---
+++ borrowed.php
@@ -0,0 +1,63 @@
+<?php include "sidebar.php"; ?>
+
+<link rel="stylesheet" href="style.css">
+
+<div class="main-content-container">
+
+
+<div class="section-header">
+    <div class="header-left">
+        <img src="lineMenu.png" class="menu-icon" alt="Menu">
+        <h2>Borrowed Books</h2>
+    </div>
+    <div class="header-right">
+        <span>Admin</span>
+        <img src="profile.png" class="profile-pic" alt="Admin Profile">
+    </div>
+</div>
+
+
+    <div class="borrowed-books-container">
+
+        <div class="book-card">
+            <img src="book-14.jpg">
+
+            <div class="book-info">
+                <p><strong>Title:</strong></p>
+                <p><strong>Author:</strong></p>
+                <p><strong>Type:</strong> (Exclusive/Non)</p>
+                <p><strong>Date Borrowed:</strong></p>
+                <p><strong>Time Borrowed:</strong></p>
+                <p><strong>Borrower ID:</strong></p>
+            </div>
+        </div>
+
+        <div class="book-card">
+            <img src="book-10.jpg">
+
+            <div class="book-info">
+                <p><strong>Title:</strong></p>
+                <p><strong>Author:</strong></p>
+                <p><strong>Type:</strong> (Exclusive/Non)</p>
+                <p><strong>Date Borrowed:</strong></p>
+                <p><strong>Time Borrowed:</strong></p>
+                <p><strong>Borrower ID:</strong></p>
+            </div>
+        </div>
+
+        <div class="book-card">
+            <img src="book-2.jpg">
+
+            <div class="book-info">
+                <p><strong>Title:</strong></p>
+                <p><strong>Author:</strong></p>
+                <p><strong>Type:</strong> (Exclusive/Non)</p>
+                <p><strong>Date Borrowed:</strong></p>
+                <p><strong>Time Borrowed:</strong></p>
+                <p><strong>Borrower ID:</strong></p>
+            </div>
+        </div>
+
+    </div>
+
+</div>
