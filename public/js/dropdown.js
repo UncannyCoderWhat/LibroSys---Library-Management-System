4944ed669aee2825c5ead9e0439102a64e060e3d
CedrickManzanilla-git <cedforgithub@gmail.com>
2026-07-24 22:08:39 +0800

    UPDATE: Improved genre dropdown

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,17 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     const dp = document.querySelector('.dropdown');
     const dpwrapper = document.querySelector('.dpwrapper');
-
+    const genres = document.querySelectorAll('.dpwrapper a');
+    
     if (dp && dpwrapper) {
         dp.addEventListener('click', function(event) {
-            event.stopPropagation(); 
+            event.stopPropagation();
             dpwrapper.classList.toggle('active');
         });
-
+        
         document.addEventListener('click', function(event) {
             if (!dpwrapper.contains(event.target) && event.target !== dp) {
                 dpwrapper.classList.remove('active');
             }
+        });
+        
+        genres.forEach(genre => {
+            genre.addEventListener('click', function(event) {
+                event.preventDefault();
+                
+                genres.forEach(g => g.classList.remove('active'));
+                this.classList.add('active');
+                
+                dpwrapper.classList.remove('active');
+            });
         });
     }
 });
